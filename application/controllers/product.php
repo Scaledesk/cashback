88180cec3057214f31bce2f1456cb27ae3852798
nitesh <singhniteshbca@gmail.com>
2015-07-27 18:01:58 +0530

    add functionality

---
+++ application/controllers/product.php
@@ -0,0 +1,39 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		//$this->load->library(array('ion_auth','form_validation'));
+		$this->load->helper(array('url','language'));
+
+		//$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+		//$this->lang->load('auth');
+	}
+
+	
+
+	public function add_product()
+  {
+      $data['title']='Admin';
+     if( $this->Product_model->add_product()){
+      echo "successful Add product";
+      //$this->load->view('templates/header.php',$data);
+      //$this->load->view('pages/add_employee.php');
+      //$this->load->view('templates/footer.php');  
+      }
+
+  }
+
+   public function delete_product($id)
+  {
+   if($this->Product_model->delete_product($id))
+    {
+    $this->employee_show();
+    }
+  }
+
+}
---
+++ application/models/product_model.php
@@ -0,0 +1,73 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Product_model extends CI_Model {
+public function __construct()
+{
+parent::__construct();
+//$this->load->library('session');
+	$this->load->database();
+}
+
+
+
+public function add_product()
+{
+
+ $data=array(
+'product_category'=>$this->input->post(''),
+'product_link'=>$this->input->post(''),
+'product_price'=>$this->input->post(''),
+'product_title'=>$this->input->post(''),
+'product_image'=>$this->input->post('')
+
+);
+
+$this->db->insert('product',$data);
+return true;
+}
+
+
+public function delete_product($id)
+	{
+		$this->db->where("product_id",$id);
+  		if($this->db->delete("product"))
+  		{
+  			return true;
+  		}		
+	}
+
+	public function show_product()
+	{
+		
+		
+		$query=$this->db->query("select * from product");
+		// echo '<pre />';
+		// print_r($query);
+		// die();  
+		
+		return $query->result();
+	}
+
+	
+
+public function update_product($id)
+{
+
+ $data=array(
+'product_category'=>$this->input->post(''),
+'product_link'=>$this->input->post(''),
+'product_price'=>$this->input->post(''),
+'product_title'=>$this->input->post(''),
+'product_image'=>$this->input->post('')
+);
+$this->db->where(array('product_id'=> $id));
+$this->db->update('product',$data);
+return true;
+}
+
+
+
+
+
+	
+
+}
