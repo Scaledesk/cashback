4bb856bc65afc5aea684868fd9c6ded9cabc2559
Javed101 <javedahamad4@gmail.com>
2015-07-28 14:17:25 +0530

    edited coupon control and model

--- application/controllers/coupon_control.php
+++ application/controllers/coupon_control.php
@@ -6,7 +6,7 @@ class Coupon_control extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->database();
-		$this->load->model('Coopan_model');
+		$this->load->model('Coupon_model');
 		$this->load->helper(array('url','language'));
     //$this->load->library(array('ion_auth','form_validation'));
 		//$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
@@ -30,6 +30,7 @@ class Coupon_control extends CI_Controller {
     $this->load->view('templates/footer.php');
   }
   }
+
   Public function view_coupon()
   {
     $data['title']='Add Coupon';
@@ -37,5 +38,35 @@ class Coupon_control extends CI_Controller {
     $this->load->view('templates/header.php',$data);
     $this->load->view('pages/view_coupon',$data);
     $this->load->view('templates/footer.php');
+  }
+	public function editCoupon($id)
+  {
+    $this->load->database();
+    $data['title']='Edit Coupon Details';
+    $data['h']=$this->Coupon_model->editCoupon($id);
+    $this->load->view('templates/header.php',$data);
+    $this->load->view('pages/add_coupon.php',$data);
+    $this->load->view('templates/footer.php');
+  }
+
+  public function do_editCoupon($id)
+  {
+    $this->load->database();
+    if($this->Coupon_model->do_editCoupon($id))
+    {
+      redirect(base_url().'Coupon_control/view_coupon');
+    }
+    else{
+      echo "record not updated";
+    }
+
+  }
+
+  public function deleteCoupon($id)
+  {
+    if($this->Coupon_model->deleteCoupon($id))
+    {
+      redirect(base_url().'Coupon_control/view_coupon');
+    }
   }
 }
--- application/models/coupon_model.php
+++ application/models/coupon_model.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Product_model extends CI_Model {
+class Coupon_model extends CI_Model {
 public function __construct()
 {
 parent::__construct();
@@ -19,11 +19,45 @@ public function add_coupon()
  $this->db->insert('coupon_details',$data);
  return true;
 }
+
 public function getCouponDetails()
 {
   $query=$this->db->query("select * from coupon_details");
   return $query;
 }
+public function editCoupon($id)
+	{
+
+		$this->db->where("coupon_id",$id);
+  		$query=$this->db->get("coupon_details");
+  		return $query->result();
+	}
+
+	public function do_editCoupon($id)
+	{
+
+		$data=array(
+	 'coupon_name'=>$this->input->post('coupon_name'),
+	 'coupon_company'=>$this->input->post('coupon_company'),
+	 'coupon_code'=>$this->input->post('coupon_code'),
+	 'coupon_description'=>$this->input->post('coupon_description'),
+	 'coupon_offer'=>$this->input->post('coupon_offer'),
+	 'coupon_category_id'=>$this->input->post('coupon_category_id')
+	 );
+		$this->db->where('coupon_id', $id);
+		$this->db->update('coupon_details',$data);
+		return true;
+
+	}
+public function delete_coupon($id)
+	{
+		$this->db->where("coupon_id",$id);
+  		if($this->db->delete("coupon_details"))
+  		{
+  			return true;
+  		}
+	}
+
 
 
 }
