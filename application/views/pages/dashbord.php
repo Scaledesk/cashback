6d0e979533eb50765b5acd53ea2ebd8241cc2496
vinay2 <vk324005@gmail.com>
2015-07-28 10:04:41 +0530

    add file dashboard.php file

---
+++ application/views/pages/dashbord.php
@@ -0,0 +1,13 @@
+<!--start of middle sec-->
+  <div class="middle-sec wow fadeIn" data-wow-offset="10" data-wow-duration="2s">
+    <div id="particles">
+      <div id="not-found" class="wow fadeInDown text-center container">
+        <div class="reset">
+          <h2 class="text-primary text-uppercase">Dashboard</h2>
+          
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--end of middle sec--> 
