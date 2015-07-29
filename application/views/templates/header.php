36333e52d6b2e1aceb032aadc68b8bfe064c83f8
vinay2 <vk324005@gmail.com>
2015-07-29 14:07:41 +0530

    updated heaer.php file

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -30,6 +30,7 @@ else
 
 <!--color picker-->
 <link href="<?php echo base_url().'assets/css/jquery.simplecolorpicker.css"';?>" rel="stylesheet">
+<link href="css\mycss.css" rel="stylesheet">
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
