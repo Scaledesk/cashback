ec922f2feb6dfd446fb52f475ec03f46b4bed19a
vinay2 <vk324005@gmail.com>
2015-07-29 11:17:39 +0530

    dashbord.php updated

--- application/views/pages/dashbord.php
+++ application/views/pages/dashbord.php
@@ -1,36 +1,4 @@
-<!--start of middle sec-->
-  <div class="middle-sec wow fadeIn" data-wow-offset="10" data-wow-duration="2s">
-    <div class="page-header">
-      <div class="container text-center">
-        <h2 class="text-primary text-uppercase">Welcome Admin</h2>
 
-      </div>
-    </div>
-    <section class="container">
-      <div class="row">
-         <div class="col-sm-12 equal-height-container">
-          <div class="row">
-            <div class="col-sm-4 col-md-3 sub-data-left sub-equal">
-              <div id="sticky">
-                <section class="col-sm-12">
-                  <h5 class="sub-title text-info text-uppercase">order summary</h5>
-                  <ul class="list-group summary">
-                    <a href="<?=(base_url().'Product/product_page/')?>"><li class="list-group-item text-uppercase"><strong>Product Add</strong></li></a>
-                    <a href="<?=(base_url().'Product/update_category/')?>"><li class="list-group-item text-uppercase"><strong>Udate Category</strong></li></a>
-                    <a href="<?=(base_url().'Product/show_product/')?>"><li class="list-group-item text-uppercase"><strong>taxes (5%): <span class="pull-right">$0.45</span></strong></li></a>
-                    <a href="<?=(base_url().'Coupon_control/add_coupon/')?>"><li class="list-group-item text-uppercase"><strong>Add Coupon<span class="pull-right"></span></strong></li></a>
-                    <a href="<?=(base_url().'Coupon_control/view_coupon/')?>"><li class="list-group-item text-uppercase"><strong>View Coupon<span class="pull-right"></span></strong></li></a>
-                    <a href="<?=(base_url().'Store_control/add_store/')?>"><li class="list-group-item text-uppercase"><strong>Add Store<span class="pull-right"></span></strong></li></a>
-                    <a href="<?=(base_url().'Store_control/view_store/')?>"><li class="list-group-item text-uppercase"><strong>View Store<span class="pull-right"></span></strong></li></a>
-                  </ul>
-                </section>
-                <section class="col-sm-12">
-                  <h5 class="sub-title text-info text-uppercase">total price</h5>
-                  <div class=" summary sum js-total text-center"> <strong> $114.44</strong> </div>
-                  <button class="btn btn-block btn-default hvr-underline-from-center-default"><i class="rm-icon ion-arrow-return-left"></i> return to cart</button>
-                </section>
-              </div>
-            </div>
             <div class="col-sm-8 col-md-9 sub-data-left main-sec">
               <div class="row">
 
