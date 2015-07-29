e7fa00f715a0b95a674a88bb294308af299fcbef
Javed101 <javedahamad4@gmail.com>
2015-07-29 13:03:57 +0530

    edited add_store page

--- application/views/pages/add_store.php
+++ application/views/pages/add_store.php
@@ -1,88 +1,54 @@
-  <!--start of middle sec-->
-  <div class="middle-sec wow fadeIn" data-wow-offset="10" data-wow-duration="2s">
-    <div class="page-header">
-      <div class="container text-center">
-        <h2 class="text-primary text-uppercase">Add Store</h2>
-        
-      </div>
-    </div>
-    <section class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="inner-ad">
-            <figure><img class="img-responsive" src="<?php echo base_url().'assets/images/inner-ad.jpg';?>" width="1170" height="100" alt=""></figure>
-          </div>
-        </div>
-        <div class="col-sm-12 equal-height-container">
-          <div class="row">
-            <div class="col-sm-4 col-md-3 sub-data-left sub-equal">
-              <div id="sticky">
-                <section class="col-sm-12">
-                  <h5 class="sub-title text-info text-uppercase">order summary</h5>
-                  <ul class="list-group summary">
-                    <li class="list-group-item text-uppercase"><strong>items:<span class="pull-right"> 4</span></strong></li>
-                    <li class="list-group-item text-uppercase"><strong>subtotal:<span class="pull-right"> $8.99</span></strong></li>
-                    <li class="list-group-item text-uppercase"><strong>taxes (5%): <span class="pull-right">$0.45</span></strong></li>
-                    <li class="list-group-item text-uppercase"><strong>shipping: <span class="pull-right">$5</span></strong></li>
-                  </ul>
-                </section>
-                <section class="col-sm-12">
-                  <h5 class="sub-title text-info text-uppercase">total price</h5>
-                  <div class=" summary sum js-total text-center"> <strong> $114.44</strong> </div>
-                  <button class="btn btn-block btn-default hvr-underline-from-center-default"><i class="rm-icon ion-arrow-return-left"></i> return to cart</button>
-                </section>
-              </div>
-            </div>
+
             <div class="col-sm-8 col-md-9 sub-data-left main-sec">
-              <div class="row"> 
-                
-                
-                <!--end of breadcrumb--> 
-                
+              <div class="row">
+
+
+                <!--end of breadcrumb-->
+
                 <!--start of checkout-->
                 <div class="col-sm-12">
                   <form role="form" action="" name="" method="Post">
-                    <div class="row"> 
-                      
+                    <div class="row">
+
                       <!-- START Presonal information -->
                       <fieldset class="col-md-6">
                         <legend>Add Store Name And Link</legend>
-                        
+
                         <!-- Select Category -->
                         <div class="form-group">
                           <label class="control-label" for="address">Store Name</label>
                           <input type="text" id="" name="" class="form-control" placeholder="Name" required="">
                         </div>
-                        
+
                         <!-- Product Title -->
                         <div class="form-group">
                           <label class="control-label" for="address">Store Link</label>
                           <textarea class="form-controle" placeholder="Store Link" required="" maxlength='500' style="margin: 0px; width: 407px; height: 127px;"></textarea>
                         </div>
-                        
-                        
+
+
                       </fieldset>
-                      
-                      
+
+
                       <!-- START Payment infromation -->
                       <fieldset class="col-md-6">
                         <legend>Image And Store Desciption</legend>
-                        
+
                         <!-- Price -->
                         <div class="form-group">
                           <label class="control-label" for="address">Image</label>
                           <input type="file" id="" name="" class="form-control" placeholder="Image" required="">
                         </div>
-                        
+
                         <!-- Image -->
                         <div class="form-group">
                           <label class="control-label" for="address">Store Desciption</label>
-                          <textarea class="form-controle" placeholder="Store Desciption" required="" maxlength='500' style="margin: 0px; width: 407px; height: 127px;"></textarea> 
+                          <textarea class="form-controle" placeholder="Store Desciption" required="" maxlength='500' style="margin: 0px; width: 407px; height: 127px;"></textarea>
                         </div>
-                                                              
-                      <!-- END Payment information--> 
+
+                      <!-- END Payment information-->
                     </div>
-                    
+
                     <!-- Agree checkbox and Continue button -->
                     <div class="row">
                        <div class="col-sm-6 text-right">
@@ -91,9 +57,9 @@
                     </div>
                   </form>
                 </div>
-                
-                <!--end of checkout--> 
-                
+
+                <!--end of checkout-->
+
               </div>
             </div>
           </div>
