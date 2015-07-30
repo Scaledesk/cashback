888e2fdb859b75a3f3e557dcfd9e9c796568527e
Javed101 <javedahamad4@gmail.com>
2015-07-30 18:12:54 +0530

    edited user control

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -50,7 +50,7 @@ public function display_product(){
    $data['title']='Display Product Details';
     $data['p']= $this->User_model->display_product();
    $this->load->view('templates/header.php',$data);
-   $this->load->view('pages/display_user_product.php',$data);
+   $this->load->view('pages/user_product_display.php',$data);
   $this->load->view('templates/footer.php');
 
 }
