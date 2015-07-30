5fa584374db69050daeceac9b8f09082d4b41672
vinay2 <vk324005@gmail.com>
2015-07-30 15:58:21 +0530

    updated add_category.php

--- application/views/pages/add_category.php
+++ application/views/pages/add_category.php
@@ -1,24 +1,39 @@
 
-<!--start of middle sec-->
-  <div class="middle-sec wow fadeIn" data-wow-offset="10" data-wow-duration="2s">
-    <div id="particles">
-      <div id="not-found" class="wow fadeInDown text-center container">
-        <div class="reset">
-          <h2 class="text-primary text-uppercase">Add Category</h2>
-          <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-          <form method="post" id="track-order" action="<?=(base_url().'Product/add_category/')?>" method="post" accept-charset="UTF-8">
-            <div class=" form-group">
-              <label class="sr-only" for="order-id">Category Title</label>
-              <input type="text" class="form-control" value="" name="category_title" id="order-id" placeholder="Category Title" required="">
+<!--start of checkout-->
+                <div class="col-sm-12">
+                  <form role="form" action="" name="" method="Post">
+                    <div class="row-center"> 
+                      
+                      <div class="col-sm-8">
+                        <fieldset>
+                          <legend>Categories Title</legend>
+                          <input type="text" maxlength="60" name="" id="ccv" class="form-control">
+                          <hr>
+                        </fieldset>
+                      </div>
+                    <div class="row-center">
+                      <div class="col-sm-8">
+                        <fieldset>
+                          <legend>Categories Descritpion</legend>
+                          <textarea class="form-control" rows="5" cols="40" name="" id="" required=""></textarea>
+                          <hr>
+                        </fieldset>
+                      </div>
+                      
+                      <div class="col-sm-6 text-right">
+                        <button class="btn btn-primary hvr-underline-from-center-primary " type="button">complete checkout</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                
+                <!--end of checkout--> 
+                
+              </div>
             </div>
-            <div class=" form-group">
-              <label class="sr-only" for="billing-mail">Category Description</label>
-              <textarea class="form-control" rows="5" cols="40" placeholder="Category Description" name="category_description" id="" required=""></textarea>
-            </div>
-            <input type="submit" class="btn btn-primary hvr-underline-from-center-primary " value="Submit">
-          </form>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
-  <!--end of middle sec-->
+  <!--end of middle sec--> 
