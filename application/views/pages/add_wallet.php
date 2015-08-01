9a052aba090863e09ffccad80aeaf214099ac1be
vinay2 <vk324005@gmail.com>
2015-08-01 16:47:59 +0530

    add the the in pages add_wallet.php

---
+++ application/views/pages/add_wallet.php
@@ -0,0 +1,74 @@
+
+            <div class="col-sm-8 col-md-9 sub-data-left main-sec">
+              <div class="row">
+
+
+                <!--end of breadcrumb-->
+
+                <!--start of checkout-->
+                <div class="col-sm-12">
+                  <form role="form" action="" name="" method="Post">
+                    <div class="row">
+
+                      <!-- START Presonal information -->
+                      <fieldset class="col-md-6">
+                        <legend>User Name And Ammount</legend>
+
+                        <!-- Select Category -->
+                        <div class="form-group">
+                          <label class="control-label" for="card">User Name</label>
+                          <select class="selectpicker" id="card">
+                            <option>user1
+                            <option>user2
+                            <option>user3
+                          </select>
+                        </div>
+
+                        <!-- Product Title -->
+                        <div class="form-group">
+                          <label class="control-label" for="address">Awailable Ammount</label>
+                          <input type="text" id="" name="" class="form-control" placeholder="Name" required="">
+                        </div>
+
+
+                      </fieldset>
+
+
+                      <!-- START Payment infromation -->
+                      <fieldset class="col-md-6">
+                        <legend>Add Ammount And Total Ammount</legend>
+
+                        <!-- Price -->
+                        <div class="form-group">
+                          <label class="control-label" for="address">Add Ammount<label>
+                          <input type="file" id="" name="" class="form-control" placeholder="Image" required="">
+                        </div>
+
+                        <!-- Image -->
+                        <div class="form-group">
+                          <label class="control-label" for="address">Total Ammount</label>
+                          <input type="text" id="" name="" class="form-control" placeholder="Name" required="">
+                        </div>
+
+                      <!-- END Payment information-->
+                    </div>
+
+                    <!-- Agree checkbox and Continue button -->
+                    <div class="row">
+                       <div class="col-sm-6 text-right">
+                        <input type="submit" class="btn btn-primary hvr-underline-from-center-primary " value="Submit">
+                      </div>
+                    </div>
+                  </form>
+                </div>
+
+                <!--end of checkout-->
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <!--end of middle sec-->
