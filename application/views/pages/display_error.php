89aa95081ad737d267f6e27ae128d6da4b5921c9
niteshbca <singhniteshbca@gmail.com>
2015-09-12 19:54:38 +0530

    added new page

---
+++ application/views/pages/display_error.php
@@ -0,0 +1,23 @@
+﻿
+
+ 
+      <!--start of middle sec-->
+     
+    <div class="page-header">
+          <div class="container text-center">
+          <h2 class="text-primary text-uppercase">Error</h2>
+        
+      </div>
+        </div>
+    <section class="container equal-height-container">
+         
+        
+              <div id="map-canvas" >Result not found</div>
+           
+        
+      
+        </section>
+ 
+      <!--end of middle sec--> 
+      
+     
